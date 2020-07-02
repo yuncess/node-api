ed8bd856848944669c787854b7829b97f99c8a1c
yuncess <yuncess@163.com>
2020-07-02 17:21:35 +0800

    e

--- controllers/index.js
+++ controllers/index.js
@@ -4,18 +4,17 @@ var router = express.Router();
 const dbName = 'demo1';
 
 router.post('/add', function (req, res, next) {
-  var json = req.body.content;
+  var json = req.body;
+  console.log(req.body);
   client.connect((err) => {
     const db = client.db(dbName);
-    db.collection('babies').insertOne({ name: '小南瓜', age: 1.5 }, function (
-      error,
-      result
-    ) {
+    db.collection('babies').insertOne(json, function (error, result) {
       if (error) {
         console.log('insert database faile');
       }
       res.setHeader('Content-type', 'application/json;charset=utf-8');
-      res.end(JSON.stringify({ data: true }));
+      res.json({ data: true });
+      res.end();
       console.log('insert database successfully');
     });
   });
