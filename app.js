07d8a1209866edf8e43b362920a9a92da97881af
yuncess <yuncess@163.com>
2020-07-02 15:56:16 +0800

    Headers

--- app.js
+++ app.js
@@ -12,15 +12,20 @@ var app = express();
 
 //允许跨域
 app.all('*', function (req, res, next) {
-  var orginList = ['http://localhost:3000'];
+  var orginList = ['http://localhost:3000', 'http://localhost:3001'];
   if (orginList.includes(req.headers.origin.toLowerCase())) {
     //设置允许跨域的域名，*代表允许任意域名跨域
     res.header('Access-Control-Allow-Origin', req.headers.origin);
   }
   //允许的header类型
-  res.header('Access-Control-Allow-Headers', 'content-type');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'content-type,origin,authorization,*'
+  );
+
   //跨域允许的请求方式
   res.header('Access-Control-Allow-Methods', 'DELETE,PUT,POST,GET,OPTIONS');
+
   if (req.method.toLowerCase() == 'options') {
     res.send(200); //让options尝试请求快速结束
   } else {
