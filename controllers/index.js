76c7b90c7f2f57dc6abb9f6f1d2d58c2d47d4b7b
yuncess <yuncess@163.com>
2020-07-02 20:53:08 +0800

    login

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const jwt = require('jsonwebtoken');
 var client = require('../lib/client').client;
 var router = express.Router();
 const dbName = 'demo1';
@@ -17,6 +18,28 @@ router.post('/add', function (req, res, next) {
       res.end();
       console.log('insert database successfully');
     });
+  });
+});
+
+router.post('/quary', function (req, res, next) {
+  var params = req.body;
+  console.log(req.body);
+  client.connect((err) => {
+    const db = client.db(dbName);
+    const users = db.collection('users');
+    if (!params.name) {
+      res.send({ data: false, message: '用户名不能为空' });
+    } else {
+      users.findOne({ name: params.name }, function (error, result) {
+        if (params.psd === result.psd) {
+          let token = jwt.sign(result.name, result.psd);
+          users.updateOne({ name: params.name }, { $set: { token } });
+          res.send({ data: true });
+        } else {
+          res.send({ data: false, message: '密码错误' });
+        }
+      });
+    }
   });
 });
 
